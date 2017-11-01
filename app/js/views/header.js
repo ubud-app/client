d5c206c640fdda7f05934c94dd25e5e73493c776
Sebastian Pekarek <mail@sebbo.net>
2017-11-01 15:31:10 +0100

    HeaderView: Context Menu
    close #35

--- app/js/views/header.js
+++ app/js/views/header.js
@@ -3,6 +3,7 @@
 import $ from 'jquery';
 import gravatar from 'gravatar-url';
 import BaseView from './_';
+import AppHelper from '../helpers/app';
 import DataHelper from '../helpers/data';
 import HeaderTemplate from '../../templates/header.handlebars';
 
@@ -30,6 +31,20 @@ export default BaseView.extend({
 			$('<img />').attr({
 				src: gravatar(user.get('email'), {size: (window.devicePixelRatio || 1) * 60})
 			}).appendTo($gravatar);
+		});
+
+		const documents = await DataHelper.getDocuments().wait();
+		const $document = this.$el.find('.header_dropdown_item--document');
+		const $documentLink = $document.children('a');
+
+		this.listenToAndCall(AppHelper.router(), 'route', () => {
+			const document = documents.get(location.hash.substr(1).split('/')[0]);
+			$document.toggleClass('header_dropdown_item--hidden', !document);
+			if(!document) {
+				return;
+			}
+
+			$documentLink.attr('href', '#' + document.id + '/settings');
 		});
 	}
 });
