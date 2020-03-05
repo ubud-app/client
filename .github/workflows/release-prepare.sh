858d71c394fc9946afc3ccd643f994fcc9904a42
Sebastian Pekarek <mail@sebbo.net>
2020-03-05 22:57:38 +0100

    fix: Upload sourcmaps to sentry instance

---
+++ .github/workflows/release-prepare.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sentry-cli releases new -p client $VERSION
+sentry-cli releases set-commits $VERSION --auto
+
+PATH="$(pwd)/.devcache/zopfli:$PATH" npm run build
+
+sentry-cli releases files $VERSION upload-sourcemaps --rewrite ./dest/
